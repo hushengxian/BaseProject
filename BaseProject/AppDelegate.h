7715e4dc834ec608a40755e8132bfe874a992c01
saint <hushengxian999@163.com>
2018-11-08 16:31:54 +0800

    Initial Commit

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by Mac on 2018/11/8.
+//  Copyright © 2018年 saint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseProject
+//
+//  Created by Mac on 2018/11/8.
+//  Copyright © 2018年 saint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
